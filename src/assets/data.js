ce2479b95a1ff73a8875125eaa883b3262a9a295
kunalsharma2 <kunalsharma2oct@gmail.com>
2024-06-20 14:53:28 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question:"Which device is required for the Internet connection?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
+    },
+    { 
+        question:"Which of the following is used to pass data to a component from outside in React?",
+        option1:"setstate",
+        option2:"props",
+        option3:"usestate",
+        option4:"useeffect",
+        ans:2,
+    },
+    {
+        question:"Which method in a React class component is used to set the initial state?",
+        option1:"getInitialState",
+        option2:"getDerivedStateFromProps",
+        option3:"componentDidMount",
+        option4:"constructor",
+        ans:4,
+    },
+    {
+        question:"What is the purpose of the 'key' prop in React?",
+        option1:"To uniquely identify elements in an array",
+        option2:"To set the initial state of a component",
+        option3:"To handle events in a component",
+        option4:"To pass data from a parent component",
+        ans:1,
+    },
+    {
+        question:"What is the difference between var, let, and const in JavaScript?",
+        option1:"var is used for global variables, let is used for local variables, and const",
+        option2:"var is used for local variables, let is used for global variables, and const",
+        option3:"var is function scoped, let and const are block scoped",
+        option4:"var is block scoped, let and const are function scoped",
+        ans:3,
+    }
+];
